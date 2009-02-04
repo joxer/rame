69aa558f31ea4b107e4a5a9d5aea3122d0b9a6b8
joxer <kell.92.k@gmail.com>
2009-02-04 22:38:22 +0100

    ay

--- Classes/Method.rb
+++ Classes/Method.rb
@@ -1,17 +1,2 @@
-class GMethod
-  
-  def initialize
-  
-    
-  end
-
-  #write your method here
-
-
-
-  def hello
-
-return Erb_Handler.new('hello').run
-
-  end
+  end  end  def b  end
  end
--- Rakefile.rb
+++ Rakefile.rb
@@ -2,12 +2,13 @@ require 'yaml'
 require 'rake'
 require 'sqlite3'
 
-y = YAML::load(File.new("method.yml"))
 
+i = ENV['method']
  task :default do
     puts "puts rake --task to see avaible tasks"
   end
   
+
 
 namespace :method do
    
@@ -18,14 +19,12 @@ namespace :method do
       r = s.readlines
     end
     
-    for i in y
-      
+   
       File.open("m/#{i}.eruby", "w") do |s|
         s.puts "<!-- no comment -->"
         s.close
         
-      end
-      
+          
 
     end
     
@@ -33,10 +32,10 @@ namespace :method do
     File.open("Classes/Method.rb", "w") do |s|
       r.pop if r[-1] =~ /end/
       r.pop if r[-2] =~ /end/
-      for i in y
-        r.push("\n\n  def #{i}\n\nreturn Erb_Handler.new('#{i}').run\n\n  end")
+      
+        r.push("\n\n  def #{i}\n\n\nreturn Erb_Handler.new('#{i}').run\n\n  end")
         
-      end
+      
       r.push("\n end") 
       s.puts r.join
       
@@ -44,29 +43,27 @@ namespace :method do
       
     end
   end
-  
+=begin
   desc "This task remove method contained into method.yml to Classes/Gmethod.rb"
   task :remove do
     lines = Array.new
     File.open("Classes/Method.rb", "r") do |s|
       lines = s.readlines
       0.upto(lines.length - 1) do |f|
-        for i in y do 
-          
-          if lines[f].match("def #{i}") != nil
-            lines[f] = ""
-            f.upto(lines.length - 1) do |q|
-              
-              if lines[q].match("end") != nil
-                lines[q] = ""
-                break
-              end
-            end
-          end   
-          if lines[f].match("#{i}") != nil
+       
+        if lines[f].match("def #{i}") != nil
+          lines[f] = ""
+          f.upto(lines.length - 1) do |q|
             
-            lines[f] = ""
+            if lines[q].match("end") != nil
+              lines[q] = ""
+              break
+            end
           end
+        end   
+        if lines[f].match("#{i}") != nil
+          
+          lines[f] = ""
         end
       end
       s.close
@@ -78,18 +75,17 @@ namespace :method do
       s.close
     end
     
-    for i in y
-      begin
-        
-        ` rm m/#{i}.eruby`
-      rescue
-        
-      end
+    begin
+      
+      ` rm m/#{i}.eruby`
+    rescue
+      
+    
       
     end
     
   end
-  
+=end  
 end
 
 namespace :db do
